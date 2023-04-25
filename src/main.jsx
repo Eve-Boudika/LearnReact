55349ee5b3dcb85ab34c0aa052e328b4f821ba73
Evelyn <evelabohemia@gmail.com>
2023-04-25 16:28:09 -0300

    se agrega nueva app de prueba CounterApp

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { HelloWorldApp } from './HelloWorldApp.jsx';
+import { CounterApp } from './CounterApp.jsx';
 
 
 
@@ -10,7 +11,8 @@ import { HelloWorldApp } from './HelloWorldApp.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    <HelloWorldApp titulo="pepepepe" subtitulo="subpepepe"/>
+    {/* <App /> */}
+    {/* <HelloWorldApp titulo="pepepepe" subtitulo="subpepepe"/> */}
+    <CounterApp value={10}/>
   </React.StrictMode>,
 )
