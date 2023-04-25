6e23e4dc9af991b2fe9143b074faa07a3aa639a6
Evelyn <evelabohemia@gmail.com>
2023-04-25 17:15:42 -0300

    Se agrega boton para manejar eventos

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,10 +1,20 @@
 import { PropTypes } from 'prop-types';
+// import { Button } from '@mui/material';
+import Button from '@mui/material/Button';
+
 
 export const CounterApp = ({value}) => {
+    const handleadd = () => { 
+        console.log('+1') }
   return (
     <>
     <div>CounterApp</div>
     <h2> { value } </h2>
+
+
+    <Button onClick={ handleadd }  variant="contained" key="counterButton">
+      +1
+    </Button>
     </>
   );
 }
